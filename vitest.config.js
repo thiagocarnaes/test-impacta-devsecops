d7d1e8ff2597216047901e07e0294317d691a334
Thiago Carnaes <thiago.carnaes@gmail.com>
2025-06-30 11:05:20 -0300

    ci/cd 5

---
+++ vitest.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8', // or 'c8'
+      reporter: ['text', 'json', 'html', 'lcov'],
+      reportsDirectory: './coverage',
+      exclude: [
+        'node_modules/',
+        'dist/',
+        'coverage/',
+        '**/*.config.js',
+        '**/*.config.ts',
+        'src/main.js',
+        'src/main.ts'
+      ]
+    }
+  }
+})

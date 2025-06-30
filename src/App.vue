ced09cc3ae0dc0da4cf4f224a7dc52ff11676a12
Thiago Carnaes <thiago.carnaes@gmail.com>
2025-06-30 11:57:03 -0300

    test update files

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
     </div>
     <button
       v-if="winner"
-      class="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-gray-100"
+      class="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
       @click="resetGame"
     >
       Jogar Novamente
